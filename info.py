be7da826661409c11d2be5b6d98c2f36a17c6504
Farook13 <umarulfarook787@gmail.com>
2025-02-21 20:41:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -8,6 +8,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN',"7857321740:AAEtcoE9BbLGCaF5TlkeGvhLZpXU36vc
 # Telegram API ID and Hash from my.telegram.org
 API_ID = int(environ.get('API_ID', '12618934'))
 API_HASH = environ.get('API_HASH', '49aacd0bc2f8924add29fb02e20c8a16')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7857321740:AAEtcoE9BbLGCaF5TlkeGvhLZpXU36vco8E')
 # MongoDB Configuration
 DATABASE_URI = environ.get("mongodb+srv: //farook:farook@cluster0.dmaou.mongodb.net/")
 DATABASE_NAME = environ.get("DATABASE_NAME", "Farook")
