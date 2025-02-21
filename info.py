d5810ddd7d9f657376dfdc967062a47066bf91ab
Farook13 <umarulfarook787@gmail.com>
2025-02-21 20:32:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,14 +9,14 @@ BOT_TOKEN = environ.get('BOT_TOKEN',"7857321740:AAEtcoE9BbLGCaF5TlkeGvhLZpXU36vc
 API_ID = int(environ.get('API_ID', '12618934'))
 API_HASH = environ.get('API_HASH', '49aacd0bc2f8924add29fb02e20c8a16')
 # MongoDB Configuration
-DATABASE_URI = os.getenv("mongodb+srv: //farook:farook@cluster0.dmaou.mongodb.net/")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "Farook")
+DATABASE_URI = environ.get("mongodb+srv: //farook:farook@cluster0.dmaou.mongodb.net/")
+DATABASE_NAME = environ.get("DATABASE_NAME", "Farook")
 
 # Force Subscribe Channel ID (e.g., -100123456789)
-AUTH_CHANNEL = os.getenv("-1002256041072")
+AUTH_CHANNEL = environ.get("-1002256041072")
 
 # Log Channel ID for bot activities
-LOG_CHANNEL = os.getenv("-1002467149516")
+LOG_CHANNEL = environ.get("-1002467149516")
 
 # Bot Owner ID
-OWNER_ID = int(os.getenv("OWNER_ID", "5032034594"))
+OWNER_ID = int(environ.get("OWNER_ID", "5032034594"))
