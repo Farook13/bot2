7d8069a3af4017eab51b37b382f8b62a174e95fa
Farook13 <umarulfarook787@gmail.com>
2025-02-21 19:31:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,9 @@ import os
 # Telegram Bot Token from @BotFather
 BOT_TOKEN = os.getenv("7857321740:AAEtcoE9BbLGCaF5TlkeGvhLZpXU36vco8E")
 
+# Telegram API ID and Hash from my.telegram.org
+API_ID = int(os.getenv("12618934"))
+API_HASH = os.getenv("49aacd0bc2f8924add29fb02e20c8a16")
 # MongoDB Configuration
 DATABASE_URI = os.getenv("mongodb+srv: //farook:farook@cluster0.dmaou.mongodb.net/")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "Farook")
